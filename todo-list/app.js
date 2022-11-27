6730db2c023e653d33692c829ee11dfa701f6c05
okyungjin <okyungjin@gmail.com>
2022-11-27 23:21:50 +0900

    refactor: use fxdom in `createNewTodoByName`

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,5 +1,5 @@
 import { } from 'fxjs';
-import { } from 'fxdom';
+import { $, $el, $find } from 'fxdom';
 
 const todoInput = document.querySelector('.todo-input');
 const todoList = document.querySelector('.todo-list');
@@ -7,33 +7,24 @@ const todoItem = document.querySelector('.todo-item');
 const todoItemCheckbox = document.querySelector('.todo-item > input');
 const todoAddBtn = document.querySelector('.todo-add-btn');
 
-const convertTmplToElem = tmpl => {
-  const wrap = document.createElement('div');
-  wrap.innerHTML = tmpl;
-  return wrap.children[0];
-}
-
 const createNewTodoByName = todoName => {
   if (!todoName) {
     alert('Todo name is required.');
     return;
   }
 
-  const li = document.createElement('li');
-  li.classList.add('todo-item');
+  const li = $el(`
+    <li class="todo-item">
+      <span>${todoName}</span>
+      <input type="checkbox" />
+    </li>
+  `);
   li.addEventListener('click', toggleTodoItem);
-
-  const span = document.createElement('span');
-  span.innerHTML = todoName;
-
-  const input = document.createElement('input');
-  input.type = 'checkbox';
+  
+  const input = $find('input[type="checkbox"]', li);
   input.addEventListener('click', () => {
     input.checked = !input.checked;
   });
-
-  li.appendChild(span);
-  li.appendChild(input);
   todoList.appendChild(li);
 }
 
