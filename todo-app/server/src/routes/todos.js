d9859d44afc154f0f8a300ed0b2113fa8de6ec77
okyungjin <okyungjin@gmail.com>
2022-12-06 10:46:57 +0900

    feat: add `showDeleted` on request params

--- todo-app/server/src/routes/todos.js
+++ todo-app/server/src/routes/todos.js
@@ -5,18 +5,22 @@ const { POOL: { QUERY }} = require('../config/database');
 router.get('/', async (req, res) => {
   // TODO: process offset and limit
   // TODO: add 'show deleted' option
-  const { offset, limit } = req.params;
+  const { offset, limit, showDeleted = false } = req.params;
   const todos = await QUERY `
     SELECT *
     FROM todo
-    WHERE deleted=false
+    WHERE deleted=${showDeleted}
     ORDER BY id DESC`;
   res.send(todos);
 });
 
 router.get('/:todoId', async (req, res) => {
-  const { todoId } = req.params;
-  const todo = await QUERY `SELECT * FROM todo WHERE deleted=false AND id=${todoId} ORDER BY id DESC`;
+  const { todoId, showDeleted = false } = req.params;
+  const todo = await QUERY `
+    SELECT *
+    FROM todo
+    WHERE deleted=${showDeleted} AND id=${todoId}
+    ORDER BY id DESC`;
   res.send(todo.length ? todo[0] : {});
 });
 
