0154fea882b2468e51d447e822b0e39bc5defd4d
okyungjin <okyungjin@gmail.com>
2022-12-12 17:08:29 +0900

    feat: setup project

---
+++ boards/server/routes/posts.js
@@ -0,0 +1,9 @@
+const express = require('express');
+// const { } = require('../../modules/posts/B/api');
+
+const router = express.Router();
+
+
+router.get('/', async (req, res, next) => {
+  // do something
+});
