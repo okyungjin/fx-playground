38da14816dcfb7404c0da9481b6f84d5a3dccce9
okyungjin <okyungjin@gmail.com>
2022-12-13 11:58:44 +0900

    feat: set ejs template

--- express-test/routes/index.js
+++ express-test/routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express1',
+    html: '<h2>hi~</h2>',
+    script: 'test.js'
+  });
 });
 
 module.exports = router;
